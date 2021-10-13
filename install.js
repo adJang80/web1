48cd19d1e71cb5e7b99b65ccfad200f751a9d356
adJang80 <adJang80@gmail.com>
2021-10-13 12:08:38 +0900

    install

---
+++ install.js
@@ -0,0 +1,33 @@
+let deferredInstallPrompt = null;
+let installButton = null;
+
+window.addEventListener('load', ()=>{
+    installButton = document.getElementById("install-button");
+    installButton.addEventListener('click', installPWA);
+});
+
+window.addEventListener(
+    'beforeinstallprompt',
+    saveBeforeInstallPromptEvent
+);
+
+function saveBeforeInstallPromptEvent(evt){
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute('hidden');
+}
+
+function insinstallPWA(){
+    deferredInstallPrompt.prompt();
+
+    installButton.remove();
+
+    deferredInstallPrompt.userChoice
+    .then((choice) => {
+        if(choice.outcome === 'accepted'){
+            console.log("user accepted", choice);
+        }else{
+            console.log("user dismissed", choice);
+        }
+        deferredInstallPrompt = null;
+    });
+}
